dd61db5498481f9f32ff8cf41bfa3d61561d1b34
snagasuri <ramnag2003@gmail.com>
2025-03-15 16:14:27 -0700

    ok

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
